596f2c1714208cc35172e9fe37238fe8a638469e
aussieW <austin.j.wilson@gmail.com>
2018-01-19 13:30:27 +1100

    Update __init__.py
    First cut at converting to Padatious and dynamically catering for additional sounds.

--- __init__.py
+++ __init__.py
@@ -23,17 +23,14 @@
 
 # Import statements: the list of outside modules you'll be using in your
 # skills, whether from other files in mycroft-core or from external libraries
-from os.path import dirname, join
+from os.path import dirname, join, exists
 from os import listdir
 
-from adapt.intent import IntentBuilder
+#from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill
+from mycroft.skills.audioservice import AudioService
+
 from mycroft.util.log import getLogger
-try:
-    from mycroft.skills.audioservice import AudioService
-except:
-    from mycroft.util import play_mp3
-    AudioService = None
 
 __author__ = 'Hasinator7'
 
@@ -54,49 +51,16 @@ class NatureSoundSkill(MycroftSkill):
 
     def getPath(self, name):
         return (join(dirname(__file__), "mp3", name))
+    
     # This method loads the files needed for the skill's functioning, and
     # creates and registers each intent that the skill uses
     def initialize(self):
-        self.load_data_files(dirname(__file__))
-        self.audioservice = None
+        self.register_intent_file('play.intent', self.handle_play_intent)
         
         if AudioService:
             self.audioservice = AudioService(self.emitter)
             self.audioservice.stop()
-        else:
-            self.process.terminate()
-            self.process.wait()
         
-        river_intent = IntentBuilder("RiverIntent").\
-                         require("PlayKeyword").\
-                         require("RainyRiverKeyword").build()
-        self.register_intent(river_intent, self.handle_river_intent)
-        
-        dawn_intent = IntentBuilder("DawnIntent").\
-                         require("PlayKeyword").\
-                         require("DawnKeyword").build()
-        self.register_intent(dawn_intent, self.handle_dawn_intent)
-        
-        thunderstorm_intent = IntentBuilder("ThunderstormIntent").\
-                         require("PlayKeyword").\
-                         require("ThunderstormKeyword").build()
-        self.register_intent(thunderstorm_intent, self.handle_thunderstorm_intent)
-        
-        tropical_storm_intent = IntentBuilder("TropicalStormIntent").\
-                         require("PlayKeyword").\
-                         require("TropicalStormKeyword").build()
-        self.register_intent(tropical_storm_intent, self.handle_tropical_storm_intent)
-        
-        ocean_intent = IntentBuilder("OceanIntent").\
-                         require("PlayKeyword").\
-                         require("OceanKeyword").build()
-        self.register_intent(ocean_intent, self.handle_ocean_intent)
-        
-        rainforest_intent = IntentBuilder("RainforestIntent").\
-                         require("PlayKeyword").\
-                         require("RainforestKeyword").build()
-        self.register_intent(rainforest_intent, self.handle_rainforest_intent)
-
     # The "handle_xxxx_intent" functions define Mycroft's behavior when
     # each of the skill's intents is triggered: in this case, he simply
     # speaks a response. Note that the "speak_dialog" method doesn't
@@ -105,53 +69,19 @@ class NatureSoundSkill(MycroftSkill):
     # the method is called.
     #TODO: Loop mp3s
 
-    def handle_river_intent(self, message):
-        path = self.getPath("rainy-river.mp3")
+    def handle_play_intent(self, message):
+        self.stop()  # ???? Just in case something is already playing ????
+        sound = message.data.get('sound')
+        LOGGER.info('NatureSoundSkill: Requested sound is ' + sound)
+        sound = sound.replace(' ', '-')
+        path = self.getPath(sound + '.mp3')
+        if not exists(path):
+            self.speak('sorry, I could play that')
+            return
         if self.audioservice:
             self.audioservice.play(path, message.data['utterance'])
-        else:
-            self.process = play_mp3(path)
-        self.speak_dialog("info",{"environment":"Rainy river"})
     
-    def handle_dawn_intent(self, message):
-        path = self.getPath("dawn-chorus.mp3")
-        if self.audioservice:
-            self.audioservice.play(path, message.data['utterance'])
-        else:
-            self.process = play_mp3(path)
-        self.speak_dialog("info",{"environment":"Dawn chorus"})
-
-    def handle_thunderstorm_intent(self, message):
-        path = self.getPath("urban-thunderstorm.mp3")
-        if self.audioservice:
-            self.audioservice.play(path, message.data['utterance'])
-        else:
-            self.process = play_mp3(path)
-        self.speak_dialog("info",{"environment":"Thunderstorm"})
-
-    def handle_tropical_storm_intent(self, message):
-        path = self.getPath("tropical-storm.mp3")
-        if self.audioservice:
-            self.audioservice.play(path, message.data['utterance'])
-        else:
-            self.process = play_mp3(path)
-        self.speak_dialog("info",{"environment":"Tropical Storm"})
-
-    def handle_rainforest_intent(self, message):
-        path = self.getPath("rainforest.mp3")
-        if self.audioservice:
-            self.audioservice.play(path, message.data['utterance'])
-        else:
-            self.process = play_mp3(path)
-        self.speak_dialog("info",{"environment":"Rainforest"})
-
-    def handle_ocean_intent(self, message):
-        path = self.getPath("ocean-waves.mp3")
-        if self.audioservice:
-            self.audioservice.play(path, message.data['utterance'])
-        else:
-            self.process = play_mp3(path)
-        self.speak_dialog("info",{"environment":"Ocean waves"})
+    
     # The "stop" method defines what Mycroft does when told to stop during
     # the skill's execution. In this case, since the skill's functionality
     # is extremely simple, the method just contains the keyword "pass", which
@@ -159,9 +89,6 @@ class NatureSoundSkill(MycroftSkill):
     def stop(self):
         if self.audioservice:
             self.audioservice.stop()
-        else:
-            self.process.terminate()
-            self.process.wait()
 
 # The "create_skill()" method is used to create an instance of the skill.
 # Note that it's outside the class itself.
